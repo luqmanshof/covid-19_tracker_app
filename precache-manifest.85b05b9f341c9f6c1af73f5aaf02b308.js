3d1334572232fa01499de21ba8069aafcfa25788
Luqman Shofuleji <luqmanshof@gmail.com>
2020-05-05 13:05:05 +0100

    Updates

---
+++ precache-manifest.85b05b9f341c9f6c1af73f5aaf02b308.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1f5d9f5cf692b84818f155dfcac2c56",
+    "url": "/covid-19_tracker_app/index.html"
+  },
+  {
+    "revision": "a00c174b336650319cfa",
+    "url": "/covid-19_tracker_app/static/css/main.dca49cd0.chunk.css"
+  },
+  {
+    "revision": "a981939e332d40ea7f31",
+    "url": "/covid-19_tracker_app/static/js/2.f84d406d.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19_tracker_app/static/js/2.f84d406d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a00c174b336650319cfa",
+    "url": "/covid-19_tracker_app/static/js/main.740eddd9.chunk.js"
+  },
+  {
+    "revision": "daab949242fb88f9957f",
+    "url": "/covid-19_tracker_app/static/js/runtime-main.d79afe22.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19_tracker_app/static/media/image.d7265326.png"
+  }
+]);
