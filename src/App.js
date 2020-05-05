0b652c97015aec031f84035cd3b9e13f0516be38
Luqman Shofuleji <luqmanshof@gmail.com>
2020-05-05 13:02:38 +0100

    files commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css'
+import { fetchData } from './api'
+
+import coronaImage from './images/image.png';
+
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData });
+  }
+
+  handleCountryChange = async (country) => {
+    // fetch data
+    const fetchedData = await fetchData(country);
+
+    // set the state
+    this.setState({ data: fetchedData, country: country });
+  }
+
+  render() {
+    const { data, country } = this.state
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={coronaImage} alt="COVID-19" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+/*
+function App2() {
   return (
     <div className="App">
       <header className="App-header">
@@ -22,5 +62,4 @@ function App() {
     </div>
   );
 }
-
-export default App;
+*/
